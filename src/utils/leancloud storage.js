2988d27311046a938dc4529d25a285b0a02bc28e
Jesse <jesse@guojiang.tv>
2019-04-11 18:52:40 +0800

    layout

---
+++ src/utils/leancloud storage.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+var APP_ID = 'wWyRLe7DuOdsvwWN4cI11DC3-gzGzoHsz'
+var APP_KEY = 'iIgXsiRVMHvOdLRoJpMSoYvm'
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
+ export default AV
